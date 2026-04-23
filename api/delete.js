8dd540a6f1a709ca8e6ae807d9214cf7f0983b0a
Jonfwilleford <jonfwilleford@gmail.com>
2026-04-23 16:43:42 -0500

    ebay changes

--- api/delete.js
+++ api/delete.js
@@ -1,5 +1,18 @@
+import crypto from "crypto";
+
 export default function handler(req, res) {
-  return res.status(200).json({
-    verificationToken: process.env.EBAY_VERIFICATION_TOKEN
+  const challengeCode = req.query.challenge_code;
+
+  const verificationToken = process.env.EBAY_VERIFICATION_TOKEN;
+
+  const endpoint = "https://mondak-hobbies.vercel.app/api/delete";
+
+  const hash = crypto
+    .createHash("sha256")
+    .update(challengeCode + verificationToken + endpoint)
+    .digest("hex");
+
+  res.status(200).json({
+    challengeResponse: hash
   });
 }
